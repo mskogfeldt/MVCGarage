1a880974644273b93c0d68d392ff9153c67f3acc
johanneswf <johanneswf@gmail.com>
2022-07-08 10:51:16 +0200

    Add IndexViewModel

---
+++ MVCGarage/Models/ViewModels/ParkedVehicleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MVCGarage.Models.Entities;
+
+
+namespace MVCGarage.Models.ViewModels
+{
+    public class ParkedVehicleViewModel
+    {
+        public int Id { get; set; }
+        public VehicleType Type { get; set; }
+        public string? RegistrationNumber { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+    }
+}
