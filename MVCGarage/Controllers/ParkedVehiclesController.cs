e6d791c50ef9fc44398b27866f4a479ab8b6bfed
reenhilm <christian@kajal.se>
2022-07-08 14:32:32 +0200

    Added Type also should save on Post-Edit

--- MVCGarage/Controllers/ParkedVehiclesController.cs
+++ MVCGarage/Controllers/ParkedVehiclesController.cs
@@ -131,6 +131,7 @@ namespace MVCGarage.Controllers
                     ParkedVehicleOriginal.RegistrationNumber = cvm.RegistrationNumber;
                     ParkedVehicleOriginal.Brand = cvm.Brand;
                     ParkedVehicleOriginal.Color = cvm.Color;
+                    ParkedVehicleOriginal.Type = cvm.Type;
 
                     //TODO ModelState(ParkedVehicleOriginal).IsValid ???
 
