cb467599cec6e21237633a534a8d1e610e832a62
johanneswf <johanneswf@gmail.com>
2022-07-08 10:51:24 +0200

    Add ChangeViewModel

---
+++ MVCGarage/Models/ViewModels/ChangeViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCGarage.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCGarage.Models.ViewModels
+{
+    [Index(nameof(RegistrationNumber), IsUnique = true)]
+    public class ChangeViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public Color Color { get; set; }
+        [Required]
+        public VehicleType Type { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string? RegistrationNumber { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string? Brand { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string? Model { get; set; }
+        [Range(0, int.MaxValue)]
+        public int WheelCount { get; set; }
+        [Required]
+        public DateTime ArrivalTime { get; set; }
+
+    }
+}
