7022b6f5da53eaf05f36c0f07583da3cafa000f6
reenhilm <christian@kajal.se>
2022-07-08 10:46:23 +0200

    Added ReceiptViewModel

---
+++ MVCGarage/Models/ViewModels/ReceiptViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MVCGarage.Models.Entities;
+
+namespace MVCGarage.Models.ViewModels
+{
+    public class ReceiptViewModel
+    {
+        public VehicleType Type { get; set; }
+        public string? RegistrationNumber { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime CheckoutTime { get; set; }
+        public DateTime TotalParkedTime { get; set; }
+        public decimal Price { get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public Color Color { get; set; }
+    }
+}
