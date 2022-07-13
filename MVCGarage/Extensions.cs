638f4812dffb3e9d134157ef608586feab2799c8
reenhilm <christian@kajal.se>
2022-07-13 14:14:06 +0200

    Revert "Replaced WhereIf extension method in Index-method"
    This reverts commit 50827d23d0c95a4bb982d1fa9671a17da869877f.

--- MVCGarage/Extensions.cs
+++ MVCGarage/Extensions.cs
@@ -4,6 +4,15 @@ namespace MVCGarage
 {
     public static class LinqExtensions
     {
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> whereClause)
+        {
+            if (condition)
+            {
+                return query.Where(whereClause);
+            }
+            return query;
+        }
+
         public static IEnumerable<T> OrderAscOrDesc<T, Tkey>(this IEnumerable<T> query, bool desc, Expression<Func<T, Tkey>> keySelector)
         {            
             if (desc)
