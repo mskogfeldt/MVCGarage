a560ad3f842ea174f0f142f33d3ce8551f5cd324
reenhilm <christian@kajal.se>
2022-07-14 09:18:23 +0200

    errormessage reset with JS after registrationNumber-change since other client-side validation behaves that way

---
+++ MVCGarage/wwwroot/js/clientValidationReset.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener("DOMContentLoaded", function (event) {
+    let regnr = document.querySelector('#RegistrationNumber');
+    regnr.addEventListener("change", function () {
+        document.getElementById('ErrorSpan').innerHTML = '';
+    });
+});
