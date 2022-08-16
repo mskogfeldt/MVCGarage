42523ca660f807bf2592a7721f8f26e03d143e15
reenhilm <christian@kajal.se>
2022-08-16 09:35:53 +0200

    Moved validationfile

--- MVCGarage/Models/ViewModels/AddVehicleViewModel.cs
+++ MVCGarage/Models/ViewModels/AddVehicleViewModel.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MVCGarage.Models.Entities;
+using MVCGarage.Validations;
 using System.ComponentModel.DataAnnotations;
 
 namespace MVCGarage.Models.ViewModels
@@ -51,37 +52,5 @@ namespace MVCGarage.Models.ViewModels
         public int WheelCount { get; set; }
 
         public bool AddSuccess { get; set; } = false;
-    }
-
-    public class RequiredIfVehicleTypeIdIs0AndStringLength : ValidationAttribute
-    {
-        public RequiredIfVehicleTypeIdIs0AndStringLength(int stringLength) => StringLength = stringLength;
-
-        public int StringLength { get; }
-
-        public string GetErrorMessageRequired() =>
-            $"A Vehicle Type Name is needed when creating a new Vehicle Type.";
-
-        public string GetErrorMessageStringLength() =>
-            $"Vehicle Type Name should not exceed {StringLength} characters.";
-
-        protected override ValidationResult? IsValid(
-            object? value, ValidationContext validationContext)
-        {
-            var AddVehicleViewModel = (AddVehicleViewModel)validationContext.ObjectInstance;
-
-            if (AddVehicleViewModel.VehicleTypeId == 0 && string.IsNullOrEmpty(value as string))
-            {
-                return new ValidationResult(GetErrorMessageRequired());
-            }
-            else if (AddVehicleViewModel.VehicleTypeId == 0)
-            {
-                string s = (string)value!;
-                if(s.Length > StringLength)
-                    return new ValidationResult(GetErrorMessageStringLength());
-            }
-
-            return ValidationResult.Success;
-        }
     }
 }
